ace69f04b20c82edc90505ba7e1454bbef124880
Aman chauhan <138082575+amanchauhan2640@users.noreply.github.com>
2025-11-08 19:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (30.77%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+         if (n == 0)
+      return 0;
+    // x is the largest 2^k <= n.
+    // x | x >> 1 -> x >> 1 needs 1 op.
+    //     x >> 1 -> 0      needs x = 2^k - 1 ops.
+    int x = 1;
+    while (x * 2 <= n)
+      x <<= 1;
+    return minimumOneBitOperations(n ^ (x | x >> 1)) + 1 + x - 1;
+    }
+};
